0f6e90bfe19ed335c3fec5b573012747b872d4ae
Chan <ieejo716@naver.com>
2023-07-13 22:18:52 +0900

    HomeController
    권한에 따른 뷰를 출력하기 위한 controller

--- Basic/src/main/java/com/example/basic/controller/HelloController.java
+++ Basic/src/main/java/com/example/basic/controller/HelloController.java
@@ -1,12 +1,25 @@
 package com.example.basic.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HelloController {
-    @GetMapping("/hello")
+
+    @GetMapping
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
     }
 }
